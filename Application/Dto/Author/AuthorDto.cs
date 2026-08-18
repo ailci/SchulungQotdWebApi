cddfdd5cd1f724223178f05b167b1028b3f8df6e
Ali Ilci <Ali.Ilci@gmx.de>
2026-08-18 11:03:55 +0200

    Author

---
+++ Application/Dto/Author/AuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dto.Author;
+
+public class AuthorDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public byte[]? Photo { get; set; }
+    public string? PhotoMimeType { get; set; }
+}

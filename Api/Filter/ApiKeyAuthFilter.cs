2eebac9a0783bdd351b22d04dd872a6c59a73662
Ali Ilci <Ali.Ilci@gmx.de>
2026-08-19 11:28:57 +0200

    Secured

--- Api/Filter/ApiKeyAuthFilter.cs
+++ Api/Filter/ApiKeyAuthFilter.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Api.Filter;
 
-public class ApiKeyAuthFilter : IAuthorizationFilter
+public class ApiKeyAuthFilter(IConfiguration configuration) : IAuthorizationFilter
 {
     private const string ApiKeyName = "x-api-key";
 
@@ -20,6 +20,20 @@ public class ApiKeyAuthFilter : IAuthorizationFilter
                 Detail = "Api Key fehlt!"
             });
             return;
+        }
+
+        //2. Fehlerfall Falscher ApiKey
+        var apiKeyFromConfig = configuration.GetValue<string>(ApiKeyName);
+
+        if (!apiKeyToValidate.Equals(apiKeyFromConfig))
+        {
+            context.Result = new UnauthorizedObjectResult(new ProblemDetails
+            {
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                Title = "An error occurred",
+                Status = StatusCodes.Status401Unauthorized,
+                Detail = "Ungültiger Api Key!"
+            });
         }
     }
 }
